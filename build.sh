02bd966444ef7cec05df9a85530447d47ba2736e
Taylor Jadin <jadin.taylor@gmail.com>
2024-01-04 06:54:03 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd /tmp
-
 # Find the latest version of ImageMagick
 function get_latest {
 curl --silent "https://api.github.com/repos/ImageMagick/ImageMagick/releases/latest" |
@@ -10,6 +8,9 @@ sed -E 's/.*"([^"]+)".*/\1/' |
 sed 's/v//'
 }
 LATEST=`get_latest`
+
+# Get rid of the existing version of ImageMagick
+rm -rf /opt/imagemagick
 
 # Clone the ImageMagick repo
 git clone https://github.com/ImageMagick/ImageMagick.git ImageMagick-$LATEST
@@ -20,6 +21,9 @@ cd ImageMagick-$LATEST
 make
 make install
 
+# Clean up the ImageMagick Repo
+rm -rf ImageMagick-$LATEST
+
 # Make the tarball
 cd /opt/imagemagick
-tar -cvzf /tmp/imagemagick.tar.gz .
+tar -cvzf imagemagick.tar.gz .
